6c27cf4d52c606b7f042a7839c40dfa78a29594c
mpmlj <dennis.oharlem@gmail.com>
2016-10-01 21:35:00 -0400

    14-2, 14-3

---
+++ cci/14_databases/14-3.sql
@@ -0,0 +1,8 @@
+# 14.3 Close All Requests: Building #11 is undergoing a major renovation.
+# Implement a query to close all requests from apartments in this building.
+
+UPDATE Requests
+SET Status = 'Closed'
+WHERE AptID IN (SELECT AptID
+                FROM Apartments
+                WHERE BuildingID = 11)
